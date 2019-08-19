e0531db857a8f94130ca2b19977b0dc46ceb576c
nagamiya  <appngmy@gmail.com>
2019-08-19 22:01:26 +0900

    topに時刻表示、ホバーがうまくいかない（1秒ごとに時刻表示切り替えてるせいだと思われる）

--- js/sample.js
+++ js/sample.js
@@ -1,16 +1,46 @@
-var time = new Date();
-//alert(time);
-var hour = time.getHours();
-//alert(hour);
+var nowTime, nowYear, nowMonth, nowDate, nowHour, nowMin, nowSec;
 $(document).ready(function () {
     index_gradient();
+
+    setInterval('showClock1()', 1000);
 });
+
+$(function () {
+    $('#top-contents').mouseover(
+        function () {
+            $("#RealtimeClockArea").text("時間帯によってグラデーションが変わるよ")
+        }
+    );
+});
+
+function showClock1() {
+    nowTime = new Date();
+    /*alert(nowTime);*/
+    nowYear = nowTime.getFullYear();
+    nowMonth = nowTime.getMonth() + 1;
+    nowDate = nowTime.getDate();
+    nowHour = set2fig(nowTime.getHours());
+    nowMin = set2fig(nowTime.getMinutes());
+    nowSec = set2fig(nowTime.getSeconds());
+    msg1 = nowYear + "/" + nowMonth + "/" + nowDate;
+    msg2 = nowHour + ":" + nowMin + ":" + nowSec;
+    //alert(msg);
+    document.getElementById("RealtimeClockArea").innerHTML = msg1 + " " + msg2;
+}
+
+// 桁数が1桁だったら先頭に0を加えて2桁に調整する
+function set2fig(num) {
+    var ret;
+    if (num < 10) { ret = "0" + num; }
+    else { ret = num; }
+    return ret;
+}
 
 function index_gradient() { 
     var topmain = document.getElementById("top-main")
     //以下switch文めっちゃうましか、mt_sgi許すまじ
     //せめて配列とかにグラデ数値入れておいて、時間をインデックスとして暑かった方がましだったよね
-    switch (hour) {
+    switch (new Date().getHours()) {
         //朝
         case 5:
         case 6:
@@ -50,3 +80,4 @@ function index_gradient() {
             break;
     }
 }
+
